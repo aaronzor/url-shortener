79973bb36c14158d685e9e629678bd57693cabdb
aaron <mcmaaron@gmail.com>
2022-03-29 00:34:37 +0100

    Password hashing & JWT signing in User Model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -15,8 +18,10 @@ const UserSchema = new mongoose.Schema({
         ]
     },
     role: {
+        /* Applications where user role grants access to different API routes
+        may make use of this - eg: 'admin' roles. Advise using 'enum' to allow 
+        a selection of roles, depending on your application */
         type: String,
-        enum: ['user', 'publisher'],
         default: 'user'
     },
     password: {
@@ -25,10 +30,51 @@ const UserSchema = new mongoose.Schema({
         minlength: 6,
         select: false
     },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
     createdAt: {
         type: Date,
         default: Date.now
     }
 });
+
+// Encrypt password using bcrypt, salts hash for added strength
+UserSchema.pre('save', async (next) => {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = () => {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async (enteredPassword) => {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = () => {
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    // Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+};
 
 export default mongoose.model('User', UserSchema);
