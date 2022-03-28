bdef096e01d1c3b15732a61a8ebda7eed1c4e593
aaron <mcmaaron@gmail.com>
2022-03-29 00:03:49 +0100

    Basic User Model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    },
+    role: {
+        type: String,
+        enum: ['user', 'publisher'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default mongoose.model('User', UserSchema);
