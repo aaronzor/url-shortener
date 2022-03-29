d3e533bb01d46f35243050916becd23a134780d4
aaron <mcmaaron@gmail.com>
2022-03-29 23:47:49 +0100

    Import middleware to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 // Security Imports
 // Middleware Imports
+import cookieParser from 'cookie-parser';
 // Database Import
 import connectDB from './config/db.js';
 
@@ -15,9 +16,18 @@ connectDB();
 
 const app = express();
 
+// Body Parser
+app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+    console.log(
+        `Server running in ${process.env.NODE_ENV} on port ${PORT}`.red.bgGreen
+            .bold
+    )
 );
