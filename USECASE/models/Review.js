33cf0aa7e45f3eec0ca18d47c90bf29a2c14df4d
aaron <mcmaaron@gmail.com>
2022-04-02 20:46:19 +0100

    Create Review USECASE model (schema)

---
+++ USECASE/models/Review.js
@@ -0,0 +1,72 @@
+import mongoose from 'mongoose';
+
+const ReviewSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a title for the review'],
+        maxlength: 100
+    },
+    text: {
+        type: String,
+        required: [true, 'Please add some text']
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10,
+        required: [true, 'Please add a rating between 1 and 10']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    resturant: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Bootcamp',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+// Prevent user from submitting more than one review per resturant
+ReviewSchema.index({ resturant: 1, user: 1 }, { unique: true });
+
+// Static method to get avg rating and save
+ReviewSchema.statics.getAverageRating = async function (resturantId) {
+    const obj = await this.aggregate([
+        {
+            $match: { resturant: resturantId }
+        },
+        {
+            $group: {
+                _id: '$resturant',
+                averageRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+
+    try {
+        await this.model('Resturant').findByIdAndUpdate(resturantId, {
+            averageRating: obj[0].averageRating
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// Call getAverageRating after save
+ReviewSchema.post('save', function () {
+    this.constructor.getAverageRating(this.resturant);
+});
+
+// Call getAverageRating before remove
+ReviewSchema.pre('remove', function () {
+    this.constructor.getAverageRating(this.resturant);
+});
+
+export default mongoose.model('Review', ReviewSchema);
