8d0221c6f9c7ae6490d7a1491f68c44cdd8496f6
aaron <mcmaaron@gmail.com>
2022-04-03 00:14:31 +0100

    Build node seeder - populate db for dev testing

---
+++ seeder.js
@@ -0,0 +1,63 @@
+import fs from 'fs';
+import mongoose from 'mongoose';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+import Resturant from './USECASE/models/Resturant.js';
+import User from './models/User.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    //useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+// Read JSON files
+const resturants = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/resturants.json`, 'utf-8')
+);
+
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+    try {
+        await Resturant.create(resturants);
+        await User.create(users);
+        console.log('Data Imported...'.green.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// Delete data
+const deleteData = async () => {
+    try {
+        await Resturant.deleteMany();
+        await User.deleteMany();
+        console.log('Data Destroyed...'.red.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
