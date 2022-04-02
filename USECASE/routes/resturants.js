8f872faf4e3cac4dfa762bab5c51f38a608308f0
aaron <mcmaaron@gmail.com>
2022-04-03 00:36:24 +0100

    Integrate reviews with resturates by id

--- USECASE/routes/resturants.js
+++ USECASE/routes/resturants.js
@@ -9,12 +9,20 @@ import {
     resturantPhotoUpload
 } from '../controllers/resturant.js';
 
+// Import model
 import Resturant from '../models/Resturant.js';
+
+// Import review router as resource
+import { reviewRoutes } from './reviews.js';
 
 const router = express.Router();
 
+// Import middleware
 import advancedResults from '../../middleware/advancedResults.js';
 import { protect, authorize } from '../../middleware/auth.js';
+
+// Re-route into other router for reviews integration by resturant
+router.use('/:resturantId/reviews', reviewRoutes);
 
 router.route('/radius/:postalCode/:distance').get(getResturantsInRadius);
 
