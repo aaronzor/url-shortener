84bad8270099c8b54013dd178e0140ae40939775
Aaron McMullan <mcmaaron@gmail.com>
2022-05-25 01:22:25 +0100

    Install axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
-  );
-}
+const App = () => {
+  return <div className='App'>hello</div>;
+};
 
 export default App;
